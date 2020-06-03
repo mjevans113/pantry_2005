9e8ea5738f23b883a9f2f39d7581024dd76b798c
Michael Evans <michaeljevans113@gmail.com>
2020-06-03 12:09:27 -0600

    Add #enough_ingredients_for? method and associated testing

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -11,4 +11,15 @@ class Pantry
   def restock(item, quantity)
     @stock[item] += quantity
   end
+
+  def enough_ingredients_for?(recipe)
+    enough = @stock.map do |ingredient, quantity|
+      quantity <= recipe.ingredients_required[ingredient]
+    end
+    if enough.include?(false)
+      false
+    else
+      true
+    end
+  end
 end

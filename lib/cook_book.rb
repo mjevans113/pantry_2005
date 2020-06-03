1641bef6426b5618e22ccc28cbfb97415ac3f56c
Michael Evans <michaeljevans113@gmail.com>
2020-06-03 11:31:02 -0600

    Add tests for and functionality to add ingredients to Cookbook when recipe is added

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,10 +1,14 @@
 class Cookbook
-  attr_reader :recipes
+  attr_reader :recipes, :ingredients
   def initialize
     @recipes = []
+    @ingredients = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+    recipe.ingredients_required.each do |ingredient|
+      @ingredients << ingredient[0].name unless @ingredients.include?(ingredient[0].name)
+    end
   end
 end
--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/recipe'
 require './lib/cook_book'
+require './lib/ingredient'
 
 class CookbookTest < MiniTest::Test
   def test_it_exists
@@ -10,10 +11,11 @@ class CookbookTest < MiniTest::Test
     assert_instance_of Cookbook, cookbook
   end
 
-  def test_it_starts_empty
+  def test_it_has_readable_attributes
     cookbook = Cookbook.new
 
     assert_equal [], cookbook.recipes
+    assert_equal [], cookbook.ingredients
   end
 
   def test_recipes_can_be_added
@@ -26,5 +28,26 @@ class CookbookTest < MiniTest::Test
 
     cookbook.add_recipe(recipe2)
     assert_equal [recipe1, recipe2], cookbook.recipes
+  end
+
+  def test_ingredients_are_added
+    cookbook = Cookbook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    recipe2 = Recipe.new("Cheese Burger")
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal ['Cheese', 'Macaroni', 'Ground Beef', 'Bun'], cookbook.ingredients
   end
 end

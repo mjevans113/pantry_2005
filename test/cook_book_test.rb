d3d668ab35400efb4c42fa4aa4631c481f05d316
Michael Evans <michaeljevans113@gmail.com>
2020-06-03 10:59:19 -0600

    Add testing for #add_recipe

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -15,4 +15,16 @@ class CookbookTest < MiniTest::Test
 
     assert_equal [], cookbook.recipes
   end
+
+  def test_recipes_can_be_added
+    cookbook = Cookbook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+
+    cookbook.add_recipe(recipe1)
+    assert_equal [recipe1], cookbook.recipes
+
+    cookbook.add_recipe(recipe2)
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 end

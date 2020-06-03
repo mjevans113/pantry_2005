06bb47ed272e73548941f7b56d78799d15fac08b
Michael Evans <michaeljevans113@gmail.com>
2020-06-03 10:42:21 -0600

    Add more robust testing of #stock_check method

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -28,5 +28,11 @@ class PantryTest < MiniTest::Test
 
     @pantry.restock(ingredient1, 5)
     assert_equal 5, @pantry.stock_check(ingredient1)
+
+    @pantry.restock(ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(ingredient1)
+
+    @pantry.restock(ingredient2, 7)
+    assert_equal 7, @pantry.stock_check(ingredient2)
   end
 end

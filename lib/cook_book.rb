4a61b5bd40a5d6a41a3dae60f37d0829d2c192f9
Michael Evans <michaeljevans113@gmail.com>
2020-06-03 11:50:59 -0600

    Add testing for and functionality of #highest_calorie_meal method

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -11,4 +11,11 @@ class Cookbook
       @ingredients << ingredient[0].name unless @ingredients.include?(ingredient[0].name)
     end
   end
+
+  def highest_calorie_meal
+    most_calories = @recipes.map do |recipe|
+      recipe.total_calories
+    end.max
+    @recipes.find {|recipe| recipe.total_calories == most_calories}
+  end
 end
--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -50,4 +50,25 @@ class CookbookTest < MiniTest::Test
 
     assert_equal ['Cheese', 'Macaroni', 'Ground Beef', 'Bun'], cookbook.ingredients
   end
+
+  def test_it_can_return_highest_calorie_meal
+    cookbook = Cookbook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    recipe2 = Recipe.new("Cheese Burger")
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2.add_ingredient(ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal recipe2, cookbook.highest_calorie_meal
+  end
 end
